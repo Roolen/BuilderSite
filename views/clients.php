59cee9798e98e121ab783aa933102e8c8e9a34f1
Zhimovskij <zhimovskij@yandex.ru>
2020-07-08 17:39:20 +0500

    make output clients for clients page

--- views/clients.php
+++ views/clients.php
@@ -1,3 +1,13 @@
 <main class="main">
-    <?= $clients[0]["first_name"] ?>
+    <h1>Clients</h1>
+    <?php foreach ($clients as $client): ?>
+        <div class="row">
+            <div class="row-cell"> <?= $client['id'] ?> </div>
+            <div class="row-cell"> <?= $client['first_name'] ?> </div>
+            <div class="row-cell"> <?= $client['middle_name'] ?> </div>
+            <div class="row-cell"> <?= $client['last_name'] ?> </div>
+            <div class="row-cell"> <?= $client['address'] ?> </div>
+            <div class="row-cell"> <?= $client['phone'] ?> </div>
+        </div>
+    <?php endforeach ?>
 </main>
